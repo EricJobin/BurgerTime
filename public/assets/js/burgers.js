af5db35486f0f9630725990f83642793113b6b97
Eric Jobin <ejobin@gmail.com>
2019-08-15 13:22:25 -0400

    Got burgers to display, with much much help from Jen

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,62 +1,63 @@
 //Assets Javascript burgers.js
 
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
 
-    var newSleepState = {
-      sleepy: newSleep
-    };
+// $(function() {
+//   $(".change-sleep").on("click", function(event) {
+//     var id = $(this).data("id");
+//     var newSleep = $(this).data("newsleep");
 
-    // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
-      type: "PUT",
-      data: newSleepState
-    }).then(
-      function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+//     var newSleepState = {
+//       sleepy: newSleep
+//     };
 
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+//     // Send the PUT request.
+//     $.ajax("/api/cats/" + id, {
+//       type: "PUT",
+//       data: newSleepState
+//     }).then(
+//       function() {
+//         console.log("changed sleep to", newSleep);
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
 
-    var newCat = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
-    };
+//   $(".create-form").on("submit", function(event) {
+//     // Make sure to preventDefault on a submit event.
+//     event.preventDefault();
 
-    // Send the POST request.
-    $.ajax("/api/cats", {
-      type: "POST",
-      data: newCat
-    }).then(
-      function() {
-        console.log("created new cat");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+//     var newCat = {
+//       name: $("#ca").val().trim(),
+//       sleepy: $("[name=sleepy]:checked").val().trim()
+//     };
 
-  $(".delete-cat").on("click", function(event) {
-    var id = $(this).data("id");
+//     // Send the POST request.
+//     $.ajax("/api/cats", {
+//       type: "POST",
+//       data: newCat
+//     }).then(
+//       function() {
+//         console.log("created new cat");
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
 
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-});
+//   $(".delete-cat").on("click", function(event) {
+//     var id = $(this).data("id");
+
+//     // Send the DELETE request.
+//     $.ajax("/api/cats/" + id, {
+//       type: "DELETE"
+//     }).then(
+//       function() {
+//         console.log("deleted cat", id);
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+// });
