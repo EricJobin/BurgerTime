e993d8c21ad053a05e8a23758effb6f5746819d0
Eric Jobin <ejobin@gmail.com>
2019-08-14 21:21:31 -0400

    Basic File Structre, Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO burgers (burger_name) VALUES ('New Bacon-ings Burger');
+INSERT INTO burgers (burger_name) VALUES ('Yes I Cayenne Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Mission A-Corn-Plished Burger');
+INSERT INTO burgers (burger_name) VALUES ('Peas and Thank You Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Cauliflowers Cumin From Inside the House Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Roquefort Files Burger');
+INSERT INTO burgers (burger_name) VALUES ('Thank God Its Fried-Egg Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hit Me With Your Best Shallot Burger');
+INSERT INTO burgers (burger_name) VALUES ('Three Beets to the Wind Burger');
+INSERT INTO burgers (burger_name) VALUES ('Summer Thyme Burger');
+INSERT INTO burgers (burger_name) VALUES ('Itsy Bitsy Teeny Weenie Yellow Polka-Dot Zucchini Burger');
+INSERT INTO burgers (burger_name) VALUES ('Santa Claus Is Cumin to Town Burger');
+INSERT INTO burgers (burger_name) VALUES ('Winter Muensterland Burger');
+INSERT INTO burgers (burger_name) VALUES ('So Many Fennel So Little Thyme Burger');
+INSERT INTO burgers (burger_name) VALUES ('Say It Aint Cilantro Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sympathy for the Deviled Egg Burger');
+INSERT INTO burgers (burger_name) VALUES ('I Dont Bay Leaf in Magic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sit and Spinach Burger');
+INSERT INTO burgers (burger_name) VALUES ('Lets Get Frisée Burger');
+INSERT INTO burgers (burger_name) VALUES ('Free to Brie You and Me Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chorizo Your Own Adventure Burger');
+INSERT INTO burgers (burger_name) VALUES ('If Looks Could Kale Burger');
+INSERT INTO burgers (burger_name) VALUES ('Run Gorgonzola Run Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeses Is Born Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beets of Burden Burger')
+
+
+
+
+-- INSERT INTO burgers (burger_name) VALUES ('');
